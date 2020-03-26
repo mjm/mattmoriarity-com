e74b59ed25a8296844e15e82fb61ab170293a31a
Matt Moriarity <matt@mattmoriarity.com>
2020-03-25 21:55:29 -0600

    Refactor gatsby-node

--- web/gatsby-node.js
+++ web/gatsby-node.js
@@ -1,14 +1,6 @@
-const { isFuture } = require('date-fns')
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const { isFuture, format } = require('date-fns')
 
-const { format } = require('date-fns')
-
-async function createBlogPostPages(graphql, actions) {
-  const { createPage } = actions
+async function createSanityPages(graphql, { createPage }) {
   const result = await graphql(`
     {
       allSanityPost(
@@ -24,31 +16,6 @@ async function createBlogPostPages(graphql, actions) {
           }
         }
       }
-    }
-  `)
-
-  if (result.errors) throw result.errors
-
-  const postEdges = (result.data.allSanityPost || {}).edges || []
-
-  postEdges
-    .filter(edge => !isFuture(edge.node.publishedAt))
-    .forEach(edge => {
-      const { id, slug = {}, publishedAt } = edge.node
-      const dateSegment = format(publishedAt, 'YYYY/MM')
-      const path = `/blog/${dateSegment}/${slug.current}/`
-
-      createPage({
-        path,
-        component: require.resolve('./src/templates/blog-post.js'),
-        context: { id }
-      })
-    })
-}
-
-async function createMicropostPages(graphql, { createPage }) {
-  const result = await graphql(`
-    {
       allSanityMicropost(
         filter: { slug: { current: { ne: null } }, publishedAt: { ne: null } }
       ) {
@@ -62,30 +29,6 @@ async function createMicropostPages(graphql, { createPage }) {
           }
         }
       }
-    }
-  `)
-
-  if (result.errors) throw result.errors
-
-  const postEdges = (result.data.allSanityMicropost || {}).edges || []
-
-  postEdges
-    .filter(edge => !isFuture(edge.node.publishedAt))
-    .forEach(edge => {
-      const { id, slug = {} } = edge.node
-      const path = `/${slug.current}/`
-
-      createPage({
-        path,
-        component: require.resolve('./src/templates/micropost.js'),
-        context: { id }
-      })
-    })
-}
-
-async function createStaticPages(graphql, { createPage }) {
-  const result = await graphql(`
-    {
       allSanityPage(filter: { slug: { current: { ne: null } } }) {
         edges {
           node {
@@ -96,28 +39,6 @@ async function createStaticPages(graphql, { createPage }) {
           }
         }
       }
-    }
-  `)
-
-  if (result.errors) throw result.errors
-
-  const pageEdges = (result.data.allSanityPage || {}).edges || []
-
-  pageEdges.forEach(edge => {
-    const { id, slug = {} } = edge.node
-    const path = `/${slug.current}/`
-
-    createPage({
-      path,
-      component: require.resolve('./src/templates/static-page.js'),
-      context: { id }
-    })
-  })
-}
-
-async function createProjectPages(graphql, { createPage }) {
-  const result = await graphql(`
-    {
       allSanityProject(filter: { slug: { current: { ne: null } } }) {
         edges {
           node {
@@ -133,7 +54,48 @@ async function createProjectPages(graphql, { createPage }) {
 
   if (result.errors) throw result.errors
 
+  const postEdges = (result.data.allSanityPost || {}).edges || []
+  const micropostEdges = (result.data.allSanityMicropost || {}).edges || []
+  const pageEdges = (result.data.allSanityPage || {}).edges || []
   const projectEdges = (result.data.allSanityProject || {}).edges || []
+
+  postEdges
+    .filter(edge => !isFuture(edge.node.publishedAt))
+    .forEach(edge => {
+      const { id, slug = {}, publishedAt } = edge.node
+      const dateSegment = format(publishedAt, 'YYYY/MM')
+      const path = `/blog/${dateSegment}/${slug.current}/`
+
+      createPage({
+        path,
+        component: require.resolve('./src/templates/blog-post.js'),
+        context: { id }
+      })
+    })
+
+  micropostEdges
+    .filter(edge => !isFuture(edge.node.publishedAt))
+    .forEach(edge => {
+      const { id, slug = {} } = edge.node
+      const path = `/${slug.current}/`
+
+      createPage({
+        path,
+        component: require.resolve('./src/templates/micropost.js'),
+        context: { id }
+      })
+    })
+
+  pageEdges.forEach(edge => {
+    const { id, slug = {} } = edge.node
+    const path = `/${slug.current}/`
+
+    createPage({
+      path,
+      component: require.resolve('./src/templates/static-page.js'),
+      context: { id }
+    })
+  })
 
   projectEdges.forEach(edge => {
     const { id, slug = {} } = edge.node
@@ -148,8 +110,5 @@ async function createProjectPages(graphql, { createPage }) {
 }
 
 exports.createPages = async ({ graphql, actions }) => {
-  await createBlogPostPages(graphql, actions)
-  await createMicropostPages(graphql, actions)
-  await createStaticPages(graphql, actions)
-  await createProjectPages(graphql, actions)
+  await createSanityPages(graphql, actions)
 }
