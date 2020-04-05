5eaede03397fea7b8abc787edb65b8bde87cc175
Matt Moriarity <matt@mattmoriarity.com>
2020-04-05 15:45:41 -0600

    Show permalink icons for headers

--- web/src/components/serializers.js
+++ web/src/components/serializers.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import BasePortableText from '@sanity/block-content-to-react'
+import { GoLink } from 'react-icons/go'
 import Figure from './Figure'
 import TweetBlock from './tweet-block'
 import YouTubeBlock from './youtube-block'
 import CodeBlock from './code-block'
+import styles from './blog.module.scss'
 
 const BlockRenderer = props => {
   const { style = 'normal', slug = '' } = props.node
   if (/^h\d/.test(style)) {
     const Component = style
-    return <Component id={slug}>{props.children}</Component>
+    return (
+      <Component id={slug} style={{ position: 'relative' }}>
+        <a href={`#${slug}`} className={styles.anchor}>
+          <GoLink size={16} />
+        </a>
+        {props.children}
+      </Component>
+    )
   }
 
   return BasePortableText.defaultSerializers.types.block(props)
