2fbfcb9f0f4b36a18f8049a7705cf4d6cf049dd2
Matt Moriarity <matt@mattmoriarity.com>
2020-04-05 16:25:14 -0600

    Use Gatsby Link for internal links in Sanity content

--- web/src/components/serializers.js
+++ web/src/components/serializers.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import BasePortableText from '@sanity/block-content-to-react'
 import { GoLink } from 'react-icons/go'
 import Figure from './Figure'
@@ -24,6 +25,15 @@ const BlockRenderer = props => {
   return BasePortableText.defaultSerializers.types.block(props)
 }
 
+const LinkRenderer = props => {
+  const internal = /^\/(?!\/)/.test(props.mark.href)
+  if (internal) {
+    return <Link to={props.mark.href}>{props.children}</Link>
+  } else {
+    return <a href={props.mark.href}>{props.children}</a>
+  }
+}
+
 const serializers = {
   types: {
     block: BlockRenderer,
@@ -31,6 +41,9 @@ const serializers = {
     tweet: TweetBlock,
     youtube: YouTubeBlock,
     code: CodeBlock
+  },
+  marks: {
+    link: LinkRenderer
   }
 }
 
